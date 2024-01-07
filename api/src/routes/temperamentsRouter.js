fc36564b8e1b4b4bcab33f7a28bcc7b00fc0a328
pablo0261 <pablobesler@gmail.com>
2024-01-07 02:24:40 -0300

    front ok

--- api/src/routes/temperamentsRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { getTemperamentsHandler } = require("../Handlers/TemperamentsHandler");
-
-const temperamentsRouter = Router();
-
-temperamentsRouter.get("/", getTemperamentsHandler);
-
-module.exports = temperamentsRouter;
